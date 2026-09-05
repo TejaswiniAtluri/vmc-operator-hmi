bc16f0e457e64f08d78a2137c63197b3e62571d8
TejaswiniAtluri <164482987+TejaswiniAtluri@users.noreply.github.com>
2026-09-05 12:49:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,6 @@ seedDatabase();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const cors = require("cors");
 app.use(cors({
   origin: "https://vmc-operator-hmi-p7t6.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
